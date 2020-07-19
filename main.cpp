61123867dcd80d0b6d0141614d954eda45800cc1
Maurice <42690947+MoeCooper@users.noreply.github.com>
2020-07-19 05:37:31 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+struct Node {
+    std::string lastName, firstName, insuranceType, address, dateVisited, SSN;
+    struct Node *next;
+}*front = NULL, *rear = NULL;
+
+
+void enqueue(std::string lname, std::string fname, std::string ins, std::string adrs, std::string dvisit, std::string soc){
+    struct Node *t = new Node();
+    if(t==NULL){
+        std::cout << "Queue is full, sorry.\n";
+    } else {
+        t->lastName = lname;
+        t->firstName = fname;
+        t->insuranceType = ins;
+        t->address = adrs;
+        t->dateVisited = dvisit;
+        t->SSN = soc;
+        t->next = NULL;
+        if(front == NULL){
+            front = rear = t;
+        } else {
+            rear->next = t;
+            rear = t;
+        }
+    }
+}
+
+int dequeue(){
+    int x = -1;
+    struct Node *temp;
+    if(front == NULL){
+        std::cout << "The queue is empty" << std::endl;
+    } else {
+        temp = front;
+        front = front->next;
+        delete temp;
+    }
+    return x;
+}
+
+void display(){
+    struct Node *p = front;
+    while(p!=NULL){
+        std::cout <<
+        "Last name: " << p->lastName <<
+        "\nFirst name: " << p->firstName <<
+        "\nInsurance type: " << p->insuranceType <<
+        "\nAddress: " << p->address <<
+        "\nDate Visited: " << p->dateVisited <<
+        "\nSocial Security Number: " << p->SSN << std::endl;
+        p = p->next;
+        std::cout << "\n";
+    }
+}
+
+
+int main() {
+    std::string lname, fname, ins, adrs, dvisit, soc;
+    int choice;
+    do {
+        std::cout << "Menu\n";
+        std::cout << "1. Add Patient\n";
+        std::cout << "2. Dequeue\n";
+        std::cout << "3. Display\n";
+        std::cout << "4. Exit\n";
+        std::cout << "Enter choice: ";
+        std::cin >> choice;
+        switch(choice)
+        {
+        case 1:
+            std::cout << "Enter Patient Last name: ";
+            std::cin >> lname;
+            std::cout << "Enter Patient first name: ";
+            std::cin >> fname;
+            std::cout << "Enter Patient Insurance Type: ";
+            std::cin >> ins;
+            std::cout << "Enter Patient Address: ";
+            std::cin >> adrs;
+            std::cout << "Enter Patient Date visited: ";
+            std::cin >> dvisit;
+            std::cout << "Enter Patient Social SN: ";
+            std::cin >> soc;
+            enqueue(lname, fname, ins, adrs, dvisit, soc);
+            break;
+        case 2:
+            std::cout << dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        default:
+            std::cout << "Enter a valid choice";
+        }
+    }while(choice<5);
+
+  return 0;
+}
